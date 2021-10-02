6e69a8a087a0671560c25068850652ed33fb04e5
shahram1371 <shahram.faraji71@gmail.com>
2021-10-02 21:20:57 +0330

    play and pause voice from internet

--- lib/audio_player.dart
+++ lib/audio_player.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:audio_service/audio_service.dart';
-// import 'package:just_audio/just_audio.dart';
 
-class AudioPlayer extends StatefulWidget {
-  @override
-  _AudioPlayerState createState() => _AudioPlayerState();
-}
-
-class _AudioPlayerState extends State<AudioPlayer> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'playScreen.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class SecondPage extends StatefulWidget {
   @override
@@ -7,6 +8,37 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  int musicFlag = 0;
+  IconData icon = Icons.play_arrow;
+  Duration totalduration;
+  Duration position;
+  initAudio() {
+    audioPlayer.onDurationChanged.listen((updatedDuration) {
+      setState(() {
+        totalduration = updatedDuration;
+      });
+    });
+    audioPlayer.onAudioPositionChanged.listen((updatePosition) {
+      setState(() {
+        position = updatePosition;
+      });
+    });
+  }
+
+  pauseAudio() {
+    audioPlayer.pause();
+  }
+
+  stopAudio() {
+    audioPlayer.stop();
+  }
+
+  playAudio() {
+    audioPlayer.play(
+        "https://irsv.upmusics.com/99/Reza%20Marzdashti%20%7C%20Vay%20Delam%20(128).mp3?_ga=2.239241731.1627605893.1633178579-1547583649.1633178579");
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -53,7 +85,126 @@ class _SecondPageState extends State<SecondPage> {
             Expanded(
               child: ListView(
                 children: [
-                  MusicCo(),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(right: 10.0, left: 10.0),
+                          child: Container(
+                            height: 120.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15.0),
+                              color: Color(0xffF6F5F8),
+                            ),
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                // crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 25.0, top: 10),
+                                        child: Row(
+                                          children: [
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Container(
+                                                  child: Center(
+                                                      child: Text(
+                                                    "K",
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: 20.0),
+                                                  )),
+                                                  width: 70.0,
+                                                  height: 70.0,
+                                                  decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10.0),
+                                                    color: Color(0xff101047),
+                                                  ),
+                                                ),
+                                                SizedBox(
+                                                  height: 10,
+                                                ),
+                                                Text(
+                                                  "Song Name",
+                                                  style: TextStyle(
+                                                    color: Color(0xff101047),
+                                                    fontSize: 18.0,
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                            Text("Singer Name",
+                                                style: TextStyle(
+                                                    color: Color(0xff101047),
+                                                    fontSize: 15.0))
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Center(
+                                    child: TextButton(
+                                      onPressed: () {
+                                        if (musicFlag == 0) {
+                                          setState(() {
+                                            musicFlag = 1;
+                                            icon = Icons.pause;
+                                            playAudio();
+                                          });
+                                        } else {
+                                          setState(() {
+                                            musicFlag = 0;
+                                            icon = Icons.play_arrow;
+                                            pauseAudio();
+                                          });
+                                        }
+
+                                        // showModalBottomSheet(
+                                        //   isScrollControlled: true,
+                                        //   context: context,
+                                        //   builder: (context) => PlayScreen(),
+                                        // );
+                                      },
+                                      child: Container(
+                                        width: 30.0,
+                                        height: 30.0,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          border: Border.all(
+                                              color: Color(0xffFF3737)),
+                                        ),
+                                        child: Center(
+                                          child: Icon(
+                                            // Icons.play_arrow,
+                                            icon,
+                                            color: Color(0xffFF3737),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(totalduration.toString()),
+                        Text(position.toString()),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -64,101 +215,9 @@ class _SecondPageState extends State<SecondPage> {
   }
 }
 
-class MusicCo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8.0),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0, left: 10.0),
-            child: Container(
-              height: 120.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                color: Color(0xffF6F5F8),
-              ),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  // crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 25.0, top: 10),
-                          child: Row(
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    child: Center(
-                                        child: Text(
-                                      "K",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 20.0),
-                                    )),
-                                    width: 70.0,
-                                    height: 70.0,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      color: Color(0xff101047),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    "Song Name",
-                                    style: TextStyle(
-                                      color: Color(0xff101047),
-                                      fontSize: 18.0,
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Text("Singer Name",
-                                  style: TextStyle(
-                                      color: Color(0xff101047), fontSize: 15.0))
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Center(
-                      child: TextButton(
-                        onPressed: () {
-                          showModalBottomSheet(
-                            isScrollControlled: true,
-                            context: context,
-                            builder: (context) => PlayScreen(),
-                          );
-                        },
-                        child: Container(
-                          width: 30.0,
-                          height: 30.0,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            border: Border.all(color: Color(0xffFF3737)),
-                          ),
-                          child: Center(
-                            child: Icon(
-                              Icons.play_arrow,
-                              color: Color(0xffFF3737),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
+// class MusicCo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return 
+//   }
+// }
